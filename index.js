e78009feb93e75f35f70dbf1196a5c7b8326192f
Zeke Lu <lvzecai@gmail.com>
2021-05-04 14:11:32 +0800

    add demo to reproduce https://bugs.chromium.org/p/chromium/issues/detail?id=1204880

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer-core');
+
+(async () => {
+    const browser = await puppeteer.launch({executablePath: '/path/to/the/browser'});
+    const page = await browser.newPage();
+    const client = await page.target().createCDPSession();
+    await client.send('Browser.setDownloadBehavior', {
+        behavior: 'allowAndName',
+        downloadPath: '/tmp',
+        eventsEnabled: true
+    });
+    try {
+        await page.goto('https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F878144%2FREVISIONS?generation=1619830882614039&alt=media');
+    } catch (err) {
+        // ignore net::ERR_ABORTED error
+    }
+
+    await browser.close();
+})();
