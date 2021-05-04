5c95ab458e2709fb6e374a901381a3a2ec315c35
Zeke Lu <lvzecai@gmail.com>
2021-05-04 20:09:27 +0800

    Browser.setDownloadBehavior should be sent to the browser target instead of the page target

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const puppeteer = require('puppeteer-core');
 
 (async () => {
     const browser = await puppeteer.launch({executablePath: '/path/to/the/browser'});
-    const page = await browser.newPage();
-    const client = await page.target().createCDPSession();
+    const client = await browser.target().createCDPSession();
     await client.send('Browser.setDownloadBehavior', {
         behavior: 'allowAndName',
         downloadPath: '/tmp',
         eventsEnabled: true
     });
+    const page = await browser.newPage();
     try {
         await page.goto('https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F878144%2FREVISIONS?generation=1619830882614039&alt=media');
     } catch (err) {
         // ignore net::ERR_ABORTED error
+        console.log(err);
     }
 
     await browser.close();
